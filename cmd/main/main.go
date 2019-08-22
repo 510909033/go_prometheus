a34b1258b48852fa14cc4f1a498de2b49b413235
centos <centos@baotian.com>
2019-08-22 11:04:19 +0800

    更新依赖

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math/rand"
 	"time"
 	//      "github.com/prometheus/client_golang/prometheus"
 	"net/http"
@@ -31,8 +32,28 @@ func inc() {
 		Help: "inc数",
 	}
 	incCounter := prometheus.NewCounter(opts)
+	prometheus.MustRegister(incCounter)
+
+	var optsGauge = prometheus.GaugeOpts{
+		Name: "test_gauge_set",
+		Help: "gauge_set数",
+	}
+
+	gauge := prometheus.NewGauge(optsGauge)
+	prometheus.MustRegister(gauge)
+
+	histogramOpts := prometheus.HistogramOpts{
+		Name:    "test_histogram",
+		Help:    "test_histogram数",
+		Buckets: []float64{500, 1000, 2000, 4000, 8000},
+	}
+	histogram := prometheus.NewHistogram(histogramOpts)
+	prometheus.MustRegister(histogram)
+
 	for {
 		incCounter.Inc()
+		gauge.Set(float64(rand.Int63n(100000)))
+		histogram.Observe(rand.Float64() * 3128)
 		time.Sleep(time.Second)
 	}
 }
